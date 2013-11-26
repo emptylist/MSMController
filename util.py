52c4eefa23d6d2619aaaf029b68f68635459e501
emptylist <crooks1379@gmail.com>
2013-11-26 16:25:47 -0600

    Added DictDiff object

--- util.py
+++ util.py
@@ -5,5 +5,31 @@
 
 class DictDiff(object):
     def __init__(self, old_dict, new_dict):
-        pass
-    
+        self._old_dict = old_dict
+        self._new_dict = new_dict
+        self._keys_added = []
+        self._keys_removed = []
+        self._values_changed = []
+        for key in old_dict:
+            if key not in new_dict:
+                self._keys_removed.append(key)
+        for key in new_dict:
+            if key not in old_dict:
+                self._keys_added.append(key)
+        for key in old_dict:
+            self._current_key_value = new_dict.get(key,None)
+            if self._current_key_value and old_dict[key] == self._current_key_value:
+                self._values_changed.append((key, old_dict[key], new_dict[key]))
+        
+    @property
+    def changes(self):
+        self._changes = []
+        for key in self._keys_added:
+            self._changes.append(("Added", str(key), str(self._new_dict[key])))
+        for key in self._keys_removed:
+            self._changes.append(("Removed", str(key)))
+        for entry in self._values_changed:
+            self._changes.append("Changed", str(entry[0]), str(entry[1]), str(entry[2]))
+    return self._changes
+
+
