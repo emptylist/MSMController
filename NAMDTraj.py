5344c98a67727dcb655e43da1d6c3c83f31c1418
emptylist <crooks1379@gmail.com>
2013-12-02 14:13:54 -0600

    NAMDTraj created, dummy function to control basic data extracting and burn-in removal

---
+++ NAMDTraj.py
@@ -0,0 +1,9 @@
+'''Functions for manipulating trajectory files.  Currently
+relies on VMD calls through the shell, and as such is dependent on
+VMD CML access and living on a UNIX-like system.'''
+
+import NAMDConf
+
+def extract_data(Config, target = "protein noh", time_cut = 0):
+    pass
+
