e387675124162c0d109b1258132b409d1978e1c9
emptylist <crooks1379@gmail.com>
2013-12-12 14:54:55 -0600

    Project.py created empty; will be used to model and control Project information

---
+++ Project.py
@@ -0,0 +1,8 @@
+'''Module file for Project objects.  This controls the record structure that organizes the
+project and the models generated.'''
+
+class Model(object):
+    pass
+
+class Project(object):
+    pass
