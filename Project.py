8a6ea3d9eec3ff2fa8cb863d1935f1dd2b3b554a
James Crooks <crooks1379@gmail.com>
2013-12-17 21:52:49 -0600

    more updates to AbstractModel; the object's structure should be finished for v1.0

--- Project.py
+++ Project.py
@@ -4,10 +4,13 @@ project and the models generated.'''
 from datetime import datetime
 
 class AbstractModel(object):
-    def __init__(self, datasets):
+    def __init__(self, datasets, MSM_model_path = None):
         self._datasets = datasets
         self._timestamp = str(datetime.now())
-        
+        if MSM_model_path:
+            self.set_MSM_model_path(MSM_model_path)
+
+    #Properties
     @property
     def timestamp(self):
         return self.timestamp
@@ -18,6 +21,22 @@ class AbstractModel(object):
     @property
     def datasets(self):
         return datasets
+
+    @property
+    def MSM_model_path(self):
+        return self._MSM_model_path
+
+    def set_MSM_model_path(self, path):
+        self._MSM_model_path = path
+        if self._MSM_model_path:
+            #set useful MSM model information
+            pass
+
+    #Model Building
+    def build_MSM_model(self):
+        #Builds an MSM Model and then sets the path
+        #This will be complicated method and might need to be broken out in some fashion
+        pass
 
     #Dataset Handling Methods
     def register_new_dataset_pair(self, reduced_dataset_path, full_dataset_path):
@@ -51,7 +70,6 @@ class NAMDModel(AbstractModel):
     def build_samples(self):
         pass
         
-
 class Project(object):
     def __init__(self):
         pass
